db177a270498ba9504fd17a5eec51fe2ee80e3b4
Riccardo <riccardo@Riccardos-MBP.lan>
2022-02-25 18:55:01 +0100

    make env variables public

--- pages/api/subscribe.js
+++ pages/api/subscribe.js
@@ -7,9 +7,9 @@ export default async (req, res) => {
     return res.status(400).json({ error: 'Email is required' })
   }
 
-  const API_KEY = process.env.MAILCHIMP_API_KEY
-  const API_SERVER = process.env.MAILCHIMP_API_SERVER
-  const AUDIENCE_ID = process.env.MAILCHIMP_AUDIENCE_ID
+  const API_KEY = process.env.NEXT_PUBLIC_MAILCHIMP_API_KEY
+  const API_SERVER = process.env.NEXT_PUBLIC_MAILCHIMP_API_SERVER
+  const AUDIENCE_ID = process.env.NEXT_PUBLIC_MAILCHIMP_AUDIENCE_ID
   
   const url = `https://${API_SERVER}.api.mailchimp.com/3.0/lists/${AUDIENCE_ID}/members`
 
